a9eaf3eaf6667b9bdd84643730b2f456f4bdbda2
lucasvida <lucasvida@gmail.com>
2026-01-21 14:56:20 -0300

    Incluído addUser e updateUser

--- index.js
+++ index.js
@@ -19,6 +19,36 @@ const dados = await turso.execute({
 console.log(users.rows);
 console.log(dados.rows);
 
+// Inserindo dados
+
+const addUser = async ()=>{
+  try{
+    await turso.execute({
+      sql: "INSERT INTO users (nome, email, telefone) VALUES (?, ?, ?)",
+      args: ["teste", "teste@teste.com", "1234567890"],
+    });
+    console.log("User added successfully");
+  }catch(error){
+    console.log(error);
+  }
+}
+addUser();
+
+// Atualizando dados
+
+const updateUser = async ()=>{
+  try{
+    await turso.execute({
+      sql: "UPDATE users SET nome = ?, email = ?, telefone = ? WHERE id = ?",
+      args: ["teste", "teste@teste.com", "1234567890", 1],
+    });
+    console.log("User updated successfully");
+  }catch(error){
+    console.log(error);
+  }
+}
+updateUser();
+
 // Sincronização local
 
 const client = createClient({
